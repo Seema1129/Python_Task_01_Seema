275362cc126a108b2bd02585cbabe67881da1cdb
seema <seema200325@gmail.com>
2026-06-06 18:49:26 +0530

    Add files via upload

---
+++ Python_FIles/system_info.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+print("Name: seema")
+print("College: kristu jayanti university")
+print("Branch: mca")
+print("Internship: Cyber Security Summer Internship")
+print("Date & Time:", datetime.now())
